4c384af7c9ca72bc75705f3f15af998f4f017a2f
Standajagermaker <stanislav.jager@gmail.com>
2026-05-03 11:27:25 +0200

    Add leaflet css

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,13 @@
-export default function RootLayout({children}){return (<html><body>{children}</body></html>)}
+export default function RootLayout({children}){
+  return (
+    <html>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
