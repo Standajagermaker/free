1a56d4e1d550a16d2b5f9e61ae3f3a8bc81f5a36
Standajagermaker <stanislav.jager@gmail.com>
2026-05-03 11:27:57 +0200

    Add Leaflet map

---
+++ app/Map.js
@@ -0,0 +1,31 @@
+"use client";
+import { useEffect, useRef } from "react";
+import L from "leaflet";
+
+export default function Map({ ads }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const map = L.map(ref.current).setView([50.0755, 14.4378], 6);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; OpenStreetMap",
+    }).addTo(map);
+
+    ads.forEach((ad) => {
+      if (ad.lat && ad.lng) {
+        L.circle([ad.lat, ad.lng], {
+          radius: 2000,
+          color: "red",
+          fillOpacity: 0.3,
+        }).addTo(map);
+      }
+    });
+
+    return () => map.remove();
+  }, [ads]);
+
+  return <div ref={ref} style={{ height: 250, borderRadius: 12 }} />;
+}
