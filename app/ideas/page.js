738f7105f63e46d0ceda725136c5237cee0a5852
Standajagermaker <stanislav.jager@gmail.com>
2026-05-03 15:37:35 +0200

    Create page.js

---
+++ app/ideas/page.js
@@ -0,0 +1,9 @@
+async function loadEvents() {
+  try {
+    const res = await fetch("/api/events");
+    const data = await res.json();
+    setEvents(data || []);
+  } catch {
+    setEvents([]);
+  }
+}
